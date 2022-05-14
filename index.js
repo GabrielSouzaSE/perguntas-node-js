eca63d745a16c6330d87f01a348ea4703c1a70d2
GabrielSouzaSE <gabrielsouzase@gmail.com>
2022-05-14 14:20:03 -0300

    Corrigindo erro de conexão do sequelize

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+//Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão ao banco de dados com SUCESSO!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
 
 // Estou dizendo para o Express usar o EJS como View engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 // Body parser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Rotas
-app.get("/",(req, res) => {
+app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.get("/perguntar",(req, res) => {
+app.get("/perguntar", (req, res) => {
     res.render("perguntar");
 })
 
-app.post("/salvarpergunta",(req, res) => {
+app.post("/salvarpergunta", (req, res) => {
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
     res.send("Formulário recebido! titulo " + titulo + " " + " descricao " + descricao);
 });
 
-
-app.listen(8080,()=>{console.log("App rodando!");});
+app.listen(8080, () => { console.log("App rodando!"); });
