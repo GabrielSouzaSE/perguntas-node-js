e9c7a62c8dd0e34a318a927a104245f23bfc7c93
GabrielSouzaSE <gabrielsouzase@gmail.com>
2022-05-14 18:24:12 -0300

    Finalização

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const connection = require("./database/database");
 const Pergunta = require("./database/Pergunta");
 const Resposta = require("./database/Resposta");
+
 //Database
 connection
     .authenticate()
